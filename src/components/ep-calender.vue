41c98a6c05095d7b0d1dadec63f69e65d5798d96
wangjun <3113874356@qq.com>
2020-08-12 11:40:56 +0800

    日历组件初步完成

--- src/components/ep-calender.vue
+++ src/components/ep-calender.vue
@@ -14,7 +14,7 @@ export default {
   props: ["value"],
   data() {
     return {
-        //选择的日期
+      //选择的日期
       selectYear: -1,
       selectMonth: -1,
       selectDay: -1,
@@ -55,27 +55,32 @@ export default {
       let days = $date.getMonthDays(year, month);
       for (var i = 1; i <= days; i++) {
         var clazz = "click item";
-        if (year == this.selectYear && month == this.selectMonth && i == this.selectDay) {
+        if (
+          year == this.selectYear &&
+          month == this.selectMonth &&
+          i == this.selectDay
+        ) {
           clazz += " selected";
         }
-        if (year == this.todayYear && month == this.todayMonth && i == this.todayYear) {
+        if (
+          year == this.todayYear &&
+          month == this.todayMonth &&
+          i == this.todayYear
+        ) {
           clazz += " today";
         }
-        template +=`
-          <li class='
-          ${clazz} 
-          ' name='item-click' val='
-          ${i} 
-          '> 
-          ${i} 
-          </li>\n`;
+        template += `
+          <li class='${clazz}' 
+              name='item-click' val='${i}'> ${i} 
+          </li>\n
+          `;
       }
       // 后置灰色
       $date.getNextDayArray(year, month).forEach(function (val) {
         template += "<li class='none item gray'>" + val + "</li>";
       });
       template += "</ul></div>";
-      this.$refs.mycalendar.innerHTML=template;
+      this.$refs.mycalendar.innerHTML = template;
     },
     selectMonthView(year) {},
     selectYearView(year) {},
@@ -104,68 +109,67 @@ export default {
 };
 </script>
 <style lang="scss">
-    /**
+/**
 ----------------------------------------
  日历组件样式
 ----------------------------------------
 */
 
 .calendar-view {
-    width: 210px;
-    user-select: none;
-    background-color: rgb(233,239,241);
-    line-height: 1.8em;
-    text-align: center;
-    box-shadow: 1px 2px 3px #79798d;
+  width: 210px;
+  user-select: none;
+  background-color: rgb(233, 239, 241);
+  line-height: 1.8em;
+  text-align: center;
+  box-shadow: 1px 2px 3px #79798d;
 }
 
 /* 头部 */
 
-.calendar-view>.header>div {
-    display: inline-block;
-    cursor: pointer;
+.calendar-view > .header > div {
+  display: inline-block;
+  cursor: pointer;
 }
 
-.calendar-view>.header>div.title {
-    width: 165px;
+.calendar-view > .header > div.title {
+  width: 165px;
 }
 
 /* 内容 */
-.calendar-view>.container.day-view>ul>li {
-    width: 29px;
+.calendar-view > .container.day-view > ul > li {
+  width: 29px;
 }
 
-.calendar-view>.container.month-view>ul>li,
-.calendar-view>.container.year-view>ul>li {
-    width: 52px;
-    line-height: 2.4em;
+.calendar-view > .container.month-view > ul > li,
+.calendar-view > .container.year-view > ul > li {
+  width: 52px;
+  line-height: 2.4em;
 }
 
-.calendar-view>.container>ul>li {
-    display: inline-block;
-
+.calendar-view > .container > ul > li {
+  display: inline-block;
 }
 
-.calendar-view>.container>ul>li.click {
-    cursor: pointer;
+.calendar-view > .container > ul > li.click {
+  cursor: pointer;
 }
-.calendar-view>.container>ul>li.none {
-    cursor: no-drop;
-}
-
-.calendar-view>.container>ul>li.gray {
-    color: gray;
+.calendar-view > .container > ul > li.none {
+  cursor: no-drop;
 }
 
-.calendar-view>.container>ul>li.click.today {
-    background-color: rgb(251, 214, 2);
+.calendar-view > .container > ul > li.gray {
+  color: gray;
 }
 
-.calendar-view>.container>ul>li.click.selected {
-    background-color: rgb(21, 199, 243);
+.calendar-view > .container > ul > li.click.today {
+  background-color: rgb(251, 214, 2);
 }
 
-.calendar-view>.container>ul>li:hover {
-    background-color: rgb(185, 233, 245);
+.calendar-view > .container > ul > li.click.selected {
+  background-color: rgb(21, 199, 243);
+}
+
+.calendar-view > .container > ul > li:hover {
+  background-color: rgb(185, 233, 245);
 }
 </style>
